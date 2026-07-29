5e2383820cd3e06e61f82f389bfa5e940c4dd7ec
xdagiz <xdagiz@protonmail.com>
2026-07-29 04:14:28 +0300

    chore: upgrade pnpm using corepack

--- scripts/enforce-package-manager.cjs
+++ scripts/enforce-package-manager.cjs
@@ -1,19 +1,11 @@
 const userAgent = process.env.npm_config_user_agent || "";
 
 const isPnpm = userAgent.startsWith("pnpm/");
-const isNpm = userAgent.startsWith("npm/");
-const isYarn = userAgent.startsWith("yarn/");
 
-if (isYarn) {
-  console.error("This repository does not support yarn. Use npm or pnpm.");
-  console.error("Use: npm install or pnpm install");
+if (!isPnpm) {
+  console.error("This repository must be installed with pnpm.");
+  console.error("Use: corepack enable && pnpm install");
   process.exit(1);
 }
 
-if (!isPnpm && !isNpm) {
-  console.error("This repository must be installed with npm or pnpm.");
-  console.error("Use: npm install or pnpm install");
-  process.exit(1);
-}
-
-console.log(`Installing with ${isNpm ? "npm" : "pnpm"}...`);
+console.log(`Installing with pnpm...`);
